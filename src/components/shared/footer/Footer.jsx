b58c73416dfb32e3c2139771ab8443c8ea304488
DamianKR <dbautistakruz@hotmail.com>
2023-02-25 10:45:01 -0500

    Header Footer layout

--- src/components/shared/footer/Footer.jsx
+++ src/components/shared/footer/Footer.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import "./Footer.scss"
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="footer">
+      <div className="top-footer">
+        <div className="item-footer">
+          <h1>Categories</h1>
+          <span>Women</span>
+          <span>Men</span>
+          <span>Shoes</span>
+          <span>Accesories</span>
+          <span>New Arrivals</span>
+        </div>
+        <div className="item-footer">
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stores</span>
+          <span>Compare</span>
+          <span>Cookies</span>
+        </div>
+        <div className="item-footer">
+          <h1>About</h1>
+          <span>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae enim
+            blanditiis cupiditate recusandae expedita tempora, ex assumenda
+            doloribus neque, ratione soluta quia.
+          </span>
+        </div>
+        <div className="item-footer">
+          <h1>Contact</h1>
+          <span>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae enim
+            blanditiis cupiditate recusandae expedita tempora, ex assumenda
+            doloribus neque, ratione soluta quia asperiores laborum.
+          </span>
+        </div>
+      </div>
+      <div className="bottom-footer">
+        <div className="left-bottom-footer">
+          <span className="logo">Demostore</span>
+          <span className="copyright">
+            © Copyright 2023. All Rights Reserved
+          </span>
+        </div>
+        <div className="right-bottom-footer">
+          <img src="/img/paymentImage.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
